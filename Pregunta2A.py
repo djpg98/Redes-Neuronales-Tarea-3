1b06fc489146f4dc4f5e7ecf6868e6120349bb0b
Diego <djpg98@gmail.com>
2021-11-07 17:52:59 -0400

    Questions

---
+++ Pregunta2A.py
@@ -0,0 +1,17 @@
+from Dataset import MultiClassDataset
+from Adaline import AdalineLayer
+
+dataset_train = MultiClassDataset('mnist_train.csv', dict([(str(i), i) for i in range(10)]))
+dataset_train.normalize_data(lambda x: x/255)
+classifier = AdalineLayer(
+    dimension=10, 
+    input_dimension=784, 
+    activation_function=lambda x: 1 if x >= 0 else 0
+)
+
+classifier.train_layer(dataset_train, 50, 0.001, True)
+
+
+dataset_test = MultiClassDataset('mnist_test.csv', dict([(str(i), i) for i in range(10)]))
+dataset_test.normalize_data(lambda x: x/255)
+classifier.eval(dataset_test)
